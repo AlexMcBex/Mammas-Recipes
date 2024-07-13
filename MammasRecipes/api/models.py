c621fb5ef6093412006525cea130b5571854cdab
alexmcbex <alessiobelli94@gmail.com>
2024-07-13 19:12:20 -0400

    expanded recipe model

--- MammasRecipes/api/models.py
+++ MammasRecipes/api/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Recipe(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     ingredients = models.TextField()
     instructions = models.TextField()
+    difficulty = models.CharField(max_length=50, choices=[('Easy', 'Easy'), ('Medium', 'Medium'), ('Hard', 'Hard')], default='Easy')
+    prep_time = models.PositiveIntegerField(help_text='Preparation time in minutes')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
