8d38d40e816aa8493f9cff1ab8e884ac2823b84b
alexmcbex <alessiobelli94@gmail.com>
2024-07-13 18:38:21 -0400

    added recipe model

--- MammasRecipes/api/models.py
+++ MammasRecipes/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
