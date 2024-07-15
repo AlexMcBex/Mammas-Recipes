62e633207c49c7667933b5c6db1d4df4462b04fa
alexmcbex <alessiobelli94@gmail.com>
2024-07-15 10:52:29 -0400

    made User registration view

--- MammasRecipes/api/views.py
+++ MammasRecipes/api/views.py
@@ -1,7 +1,9 @@
 # from django.shortcuts import render
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import Recipe
-from .serializers import RecipeSerializer
+from .serializers import RecipeSerializer, UserSerializer
 
 # Create your views here.
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -12,3 +14,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # return super().perform_create(serializer)
         serializer.save(user=self.request.user)
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
