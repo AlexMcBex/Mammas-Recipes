6aaf95a7c7aff2b55e9493f4322c9e0dd84f1461
alexmcbex <alessiobelli94@gmail.com>
2024-07-15 10:47:27 -0400

    added user serializer

--- MammasRecipes/api/serializers.py
+++ MammasRecipes/api/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
 from .models import Recipe
+from django.contrib.auth.models import User
 
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+    class Meta:
+        model = User
         fields = '__all__'
