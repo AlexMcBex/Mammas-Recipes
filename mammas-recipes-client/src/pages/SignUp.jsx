6db3474abecaaee7e4cf10a1bd7fe9b95a0fb647
alexmcbex <alessiobelli94@gmail.com>
2024-07-17 11:39:05 -0400

    users can signup through the SignUp.jsx form

--- mammas-recipes-client/src/pages/SignUp.jsx
+++ mammas-recipes-client/src/pages/SignUp.jsx
@@ -1,43 +1,87 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import apiUrl from "../apiConfig"
+import axios from "axios"
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  })
+  const [error, setError] = useState(null)
+  const [success, setSuccess] = useState(null)
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const res = await axios.post(`${apiUrl}/register/`, formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+
+      if (res.status !== 201) {
+        throw new Error(res.data.detail || "An error occurred")
+      }
+
+      console.log(res.data)
+      setSuccess("User registered successfully")
+      setError(null)
+    } catch (err) {
+      console.log(err)
+      setSuccess(null)
+      setError(err.message)
+    }
+  }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
-    <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-    <form
-      action="
-    "
-      className="flex flex-col gap-4 "
-    >
-      <input
-        type="text"
-        placeholder="username"
-        className="border p-3 rounded-lg "
-        id="username"
-      />
-      <input
-        type="text"
-        placeholder="email"
-        className="border p-3 rounded-lg "
-        id="email"
-      />
-      <input
-        type="text"
-        placeholder="password"
-        className="border p-3 rounded-lg "
-        id="password"
-      />
-      <button className="bg-green-600 text-white p-3 rounded-lg uppercase hover:opacity-80">
-        Sign Up
-      </button>
-    </form>
-    <div>
-      <p>Have an account?</p>
-      <Link to={"/sign-in"}>
-        <span className="text-blue-700">Sign In</span>
-      </Link>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      {error && <p className="text-red-500">{error}</p>}
+      {success && <p className="text-green-500">{success}</p>}
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+          value={formData.username}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button className="bg-green-600 text-white p-3 rounded-lg uppercase hover:opacity-80">
+          Sign Up
+        </button>
+      </form>
+      <div>
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
     </div>
-  </div>
   )
 }
