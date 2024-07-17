40776513af58c15af28cb8402fc5288c1dd9f115
alexmcbex <alessiobelli94@gmail.com>
2024-07-17 10:53:30 -0400

    made SignUp.jsx UI

--- mammas-recipes-client/src/pages/SignUp.jsx
+++ mammas-recipes-client/src/pages/SignUp.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
+    <div className="p-3 max-w-lg mx-auto">
+    <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+    <form
+      action="
+    "
+      className="flex flex-col gap-4 "
+    >
+      <input
+        type="text"
+        placeholder="username"
+        className="border p-3 rounded-lg "
+        id="username"
+      />
+      <input
+        type="text"
+        placeholder="email"
+        className="border p-3 rounded-lg "
+        id="email"
+      />
+      <input
+        type="text"
+        placeholder="password"
+        className="border p-3 rounded-lg "
+        id="password"
+      />
+      <button className="bg-green-600 text-white p-3 rounded-lg uppercase hover:opacity-80">
+        Sign Up
+      </button>
+    </form>
+    <div>
+      <p>Have an account?</p>
+      <Link to={"/sign-in"}>
+        <span className="text-blue-700">Sign In</span>
+      </Link>
+    </div>
+  </div>
   )
 }
