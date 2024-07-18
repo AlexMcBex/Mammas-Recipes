d1fe48a1425837007f9ec6178e8069579e1720ab
alexmcbex <alessiobelli94@gmail.com>
2024-07-18 14:49:18 -0400

    moved Recipe index

---
+++ mammas-recipes-client/src/pages/recipes/RecipesIndex.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react'
+import { apiIndexRecipes } from '../api/recipes/indexRecipes'
+
+export default function Recipes() {
+
+  const [recipes, setRecipes] = useState([])
+
+  useEffect(() => {
+    apiIndexRecipes()
+    .then((res) => {
+      setRecipes(res.data)
+      // console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
+  return (
+    <div>
+      <h1 className='text-3xl bold'>Recipes</h1>
+      <ul>
+        {recipes.map((recipe) => {
+          return <li key={recipe.id}>{recipe.name}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
