70dd9c02f77bd3d87938e7bbc392958aa31f1cc5
alexmcbex <alessiobelli94@gmail.com>
2024-07-18 11:03:09 -0400

    modified token url

--- MammasRecipesAPI/MammasRecipesAPI/urls.py
+++ MammasRecipesAPI/MammasRecipesAPI/urls.py
@@ -21,9 +21,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView
     )
 
+from api.views import CustomTokenObtainPairView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
