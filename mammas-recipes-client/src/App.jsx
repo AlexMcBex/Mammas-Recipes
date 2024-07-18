23c2b6be8ec63b3b72a6c93360fb4984581e14ad
alexmcbex <alessiobelli94@gmail.com>
2024-07-18 15:01:07 -0400

    recipe list filtered by user

--- mammas-recipes-client/src/App.jsx
+++ mammas-recipes-client/src/App.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
-import Recipes from './pages/Recipes'
+import RecipesIndex from './pages/recipes/RecipesIndex'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
 // import SignOut from './pages/SignOut'
@@ -24,7 +24,7 @@ export default function App() {
       <main className="flex-grow p-4 bg-yellow-50">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipes" element={<RecipesIndex />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>} />
         {/* <Route path="/sign-out" element={<SignOut />} /> */}
--- mammas-recipes-client/src/pages/recipes/RecipesIndex.jsx
+++ mammas-recipes-client/src/pages/recipes/RecipesIndex.jsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react'
-import { apiIndexRecipes } from '../api/recipes/indexRecipes'
+import { apiIndexRecipes } from '../../api/recipes/indexRecipes'
+import { jwtDecode } from 'jwt-decode'
 
-export default function Recipes() {
+export default function RecipesIndex() {
 
   const [recipes, setRecipes] = useState([])
 
   useEffect(() => {
     apiIndexRecipes()
     .then((res) => {
-      setRecipes(res.data)
-      // console.log(res.data)
+      const user = jwtDecode(localStorage.getItem('token'))
+      const recipeList = res.data.filter((recipe) => recipe.user ===  user.user_id)
+      setRecipes(recipeList)
+      console.log(recipeList)
     })
     .catch((err) => {
       console.log(err)
@@ -20,6 +23,7 @@ export default function Recipes() {
     <div>
       <h1 className='text-3xl bold'>Recipes</h1>
       <ul>
+        {recipes.length < 1 && <li>No recipes yet</li>}
         {recipes.map((recipe) => {
           return <li key={recipe.id}>{recipe.name}</li>
         })}
