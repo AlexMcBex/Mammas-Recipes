8b8ee45f5f959aeaa30ac942f08842d6192061a8
alexmcbex <alessiobelli94@gmail.com>
2024-07-18 10:22:38 -0400

    fixed error in views.py

--- MammasRecipesAPI/api/views.py
+++ MammasRecipesAPI/api/views.py
@@ -3,7 +3,6 @@ from rest_framework import viewsets, permissions, status
 from rest_framework.views import APIView
 from rest_framework.decorators import action
 from rest_framework.response import Response
-gi
 from .models import Recipe
 from .serializers import RecipeSerializer, UserSerializer
 
@@ -42,4 +41,3 @@ class UserRegistrationView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    gi
