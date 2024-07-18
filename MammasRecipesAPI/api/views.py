72c753b98f100de0149ccaffdae34c7cfe7b0866
alexmcbex <alessiobelli94@gmail.com>
2024-07-18 10:19:32 -0400

    TEMPORARY: removed permissions for development purposes

--- MammasRecipesAPI/api/views.py
+++ MammasRecipesAPI/api/views.py
@@ -1,7 +1,9 @@
 # from django.shortcuts import render
 from rest_framework import viewsets, permissions, status
 from rest_framework.views import APIView
+from rest_framework.decorators import action
 from rest_framework.response import Response
+gi
 from .models import Recipe
 from .serializers import RecipeSerializer, UserSerializer
 
@@ -9,15 +11,25 @@ from .serializers import RecipeSerializer, UserSerializer
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    permission_classes= [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
 
     def perform_create(self, serializer):
-        # return super().perform_create(serializer)
         serializer.save(user=self.request.user)
 
     def perform_destroy(self, instance):
         instance.delete()
         # return Response(status=status.HTTP_204_NO_CONTENT)
+
+@action(detail=False, methods=['get'], permission_classes=[permissions.AllowAny])
+def list(self, request, *args, **kwargs):
+    queryset = self.filter_queryset(self.get_queryset())
+    page = self.paginate_queryset(queryset)
+    if page is not None:
+        serializer = self.get_serializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
+    serializer = self.get_serializer(queryset, many=True)
+    return Response(serializer.data)
+
 
 class UserRegistrationView(APIView):
     authentication_classes = [] 
@@ -29,3 +41,5 @@ class UserRegistrationView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    gi
