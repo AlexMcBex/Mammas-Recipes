0ebc0eb652eded41179248713856325779bf8e09
alexmcbex <alessiobelli94@gmail.com>
2024-07-18 16:20:38 -0400

    users can delete recipes

--- mammas-recipes-client/src/pages/recipes/RecipeShow.jsx
+++ mammas-recipes-client/src/pages/recipes/RecipeShow.jsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import apiUrl from '../../apiConfig'
-import { Link } from 'react-router-dom'
+import { Link , useNavigate} from 'react-router-dom'
 
 export default function RecipeShow() {
   const { recipeId } = useParams()
   const [recipe, setRecipe] = useState(null)
 //   console.log(recipeId)
+const navigate = useNavigate()
 
   useEffect(() => {
     axios.get(`${apiUrl}/recipes/${recipeId}/`)
@@ -35,6 +36,15 @@ export default function RecipeShow() {
         return 'bg-gray-100'
     }
   }
+
+  const handleDelete = async () =>{
+    try{
+        await axios.delete(`${apiUrl}/recipes/${recipeId}/`)
+        navigate('/recipes')
+    }catch (err){
+        console.log(err)
+    }
+  }
   return (
     <>
     <div className={`max-w-6xl mx-auto p-4 pb-10 ${getBgColor(recipe.difficulty)} rounded-lg`}>
@@ -44,9 +54,10 @@ export default function RecipeShow() {
       <p><strong>Ingredients:</strong> {recipe.ingredients}</p>
       <p><strong>Instructions:</strong> {recipe.instructions}</p>
     </div>
-    <div>
+    <div className='flex'>
     <Link className="bg-blue-500 text-white p-3 rounded-lg m-5 font-bold" to="/recipes">Back to Recipes</Link>
     <Link className="bg-yellow-700 text-white p-3 rounded-lg m-5 font-bold" to={`/recipes/${recipe.id}/edit`}>Modify Recipe</Link>
+    <button className="bg-red-600 text-white p-3 rounded-lg m-5 font-bold" onClick={handleDelete}>Delete Recipe</button>
     </div>
     </>
   )
